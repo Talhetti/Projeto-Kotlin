3758d36673c8bf9faf3232a9bf009988cf1cc0c1
Tajetti <talhettialvaro@gmail.comm>
2025-04-08 20:14:01 -0300

    Criando telas, arquivos e Fim navegacao

--- app/src/main/java/com/github/alvaro/projetobloconotas/TELAS/EditNoteActivity.kt
+++ app/src/main/java/com/github/alvaro/projetobloconotas/TELAS/EditNoteActivity.kt
@@ -1,4 +1,12 @@
 package com.github.alvaro.projetobloconotas.TELAS
 
-class EditNoteActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.alvaro.projetobloconotas.R
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.editar_nota)
+    }
 }
--- app/src/main/java/com/github/alvaro/projetobloconotas/TELAS/ViewNoteActivity.kt
+++ app/src/main/java/com/github/alvaro/projetobloconotas/TELAS/ViewNoteActivity.kt
@@ -1,12 +1,21 @@
 package com.github.alvaro.projetobloconotas.TELAS
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.github.alvaro.projetobloconotas.R
 
 class ViewNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ver_nota) // Nome correto do layout
+        setContentView(R.layout.ver_nota)
+
+        val botaoAdicionarNota: View = findViewById(R.id.meuBotao)
+        botaoAdicionarNota.setOnClickListener {
+            val intent = Intent(this, EditNoteActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
