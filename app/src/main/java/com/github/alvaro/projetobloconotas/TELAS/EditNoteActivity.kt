f868a65114e391dc867c9f62ad3a9e8a97e39c3f
Tajetti <talhettialvaro@gmail.comm>
2025-04-10 20:12:11 -0300

    Modificando a tela editar_nota

--- app/src/main/java/com/github/alvaro/projetobloconotas/TELAS/EditNoteActivity.kt
+++ app/src/main/java/com/github/alvaro/projetobloconotas/TELAS/EditNoteActivity.kt
@@ -1,6 +1,8 @@
 package com.github.alvaro.projetobloconotas.TELAS
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.github.alvaro.projetobloconotas.R
 
@@ -8,5 +10,13 @@ class EditNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.editar_nota)
+
+
+        val buttonOk = findViewById<Button>(R.id.button2)
+
+        buttonOk.setOnClickListener {
+            val intent = Intent(this, ViewNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
